e6e3921694fb28b44e411ac70292d4bd8ca17718
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-12-30 22:57:52 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,7 +1,6 @@
 script_key = "SwcQsdeFdIPSrPIWwexOORDOHIueKGHp"
 
 getgenv().Config = {
-    ["SnowstormSurvival"] = true,
     ["Mailing"] = {
         ["Usernames"] = {"BigMan_MC"},
         ["Items"] = {
@@ -17,11 +16,11 @@ getgenv().Config = {
                 ["Huge Machine Egg 4"] = 1,
             },
         },
-        ["MailAllHuges"] = false --// False will mail all new huges, True will send all huges in inventory
+        ["MailAllHuges"] = false
     },
     ["Notifications"] = {
         ["UserID"] = "667064890359545917",
         ["Webhook"] = "https://discord.com/api/webhooks/1249413828547182742/VUCfE51nqu43KBRgI1qqyhoZd7AeGZtrQgsr1AADWAABe1xSFk62t77zHTCJtGqFCzk3"
     }
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e75d31131aa5d9b2da332ee7b55d5f6b.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/90f7fe313696212c7ee38945fc957537.lua"))()
