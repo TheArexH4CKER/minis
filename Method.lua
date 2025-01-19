3a806c993c3657f719576ee13051417a93504d6f
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-01-19 11:16:18 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,17 +1,8 @@
-script_key = "SwcQsdeFdIPSrPIWwexOORDOHIueKGHp"
-
 getgenv().Config = {
+    ["EquipEnchants"] = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Strong Pets", "Coins", "Fortune"},
     ["Mailing"] = {
-        ["Usernames"] = {"BigMan_MC"},
+        ["Usernames"] = {"ProfiAzUr"},
         ["Items"] = {
-            ["Misc"] = {
-                ["Lockpick A"] = 25,
-                ["Lockpick B"] = 10,
-                ["Lockpick C"] = 3,
-            },
-            ["Lootbox"] = {
-                ["Jolly Gift"] = 5,
-            },
             ["Egg"] = {
                 ["Huge Machine Egg 4"] = 1,
             },
@@ -23,4 +14,4 @@ getgenv().Config = {
         ["Webhook"] = "https://discord.com/api/webhooks/1249413828547182742/VUCfE51nqu43KBRgI1qqyhoZd7AeGZtrQgsr1AADWAABe1xSFk62t77zHTCJtGqFCzk3"
     }
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/90f7fe313696212c7ee38945fc957537.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ad5066fbca6e5f2c374e725dfc25ffec.lua"))()
