b9ebbc25af77cb1ec8441d0ff6dc49e7ad9c285c
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-01-26 15:33:19 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -4,7 +4,7 @@ script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO";
 _G.GPROGRESS_MODE = "Hybrid"
 _G.GGFX_MODE = 1
 _G.GHATCH_CHARGED_EGGS = false
-_G.GUSE_ULTIMATES = {"Chest Spell""UFO",} -- default is Ground Pound, ultimates to try to equip at script load.
+_G.GUSE_ULTIMATES = {"Chest Spell","UFO"} -- default is Ground Pound, ultimates to try to equip at script load.
 _G.GRANK_TO = 33
 _G.GZONE_TO = 999 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
 _G.GMAX_EGG_SLOTS = 99
@@ -17,4 +17,14 @@ _G.GFRUITS = {"Apple","Banana","Orange","Rainbow","Pineapple","Watermelon"}
 _G.GWEBHOOK_USERID = "667064890359545917"
 _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1222557363874168983/cd_SGmpFvesnVoiclLSyost8Ff7Yh83JApCqFtqpENfh2A2WUJIIpuTcE7JaxTzZ3hn0"
 _G.GMAIL_RECEIVERS = {"ProfiAzUr"} -- for Huges & items
+_G.GMAIL_ITEMS = {
+  ["Daycare Egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
+  ["Secret Pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+  ["Secret Pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+  ["Secret Pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+  ["Secret Pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
+  ["Royalty Charm"] = {Class = "Charm", Id = "Royalty", MinAmount = 1},
+  ["Overload Charm"] = {Class = "Charm", Id = "Overload", MinAmount = 1},
+}
+}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
