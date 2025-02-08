ccaa7c9250c16e11f82c0b0570e42afd73c2997f
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-02-08 18:14:21 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -2,12 +2,6 @@
 -- If not sure ask info in channel
 script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO";
 _G.GPROGRESS_MODE = "Hybrid"
-_G.GDO_CANNON_TYCOON_EVENT = true
-_G.GUSE_CANNON_CONSUMABLES = true
-_G.GCONVERT_HELLFIRE_CRYSTALS = true
-_G.GUSE_HELLFIRE_CORES = true
-_G.GAUTO_TYCOON_PET_UPGRADE = true
-_G.GTYCOON_MAX_REBIRTHS = 20
 _G.GGFX_MODE = 1
 _G.GHATCH_CHARGED_EGGS = false
 _G.GUSE_ULTIMATES = {"Chest Spell","UFO"} -- default is Ground Pound, ultimates to try to equip at script load.
@@ -24,16 +18,12 @@ _G.GWEBHOOK_USERID = "667064890359545917"
 _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1222557363874168983/cd_SGmpFvesnVoiclLSyost8Ff7Yh83JApCqFtqpENfh2A2WUJIIpuTcE7JaxTzZ3hn0"
 _G.GMAIL_RECEIVERS = {"ProfiAzUr"} -- for Huges & items
 _G.GMAIL_ITEMS = {
-  ["Shiny Normal Wild Galaxy Agonys"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsShiny = true},
-  ["Shiny Golden Wild Galaxy Agonys"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsGold = true, IsShiny = true},
-  ["Shiny RB Wild Galaxy Agonys"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsRainbow = true, IsShiny = true},
-  ["Hellfire Gifts"] = {Class = "Lootbox", Id = "Hellfire Gift", MinAmount = 50},
-  ["Hype Egg 2"] = {Class = "Lootbox", Id = "Hype Egg 2", MinAmount = 1},
-  ["Hype Egg"] = {Class = "Lootbox", Id = "Hype Egg", MinAmount = 1},
-  ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
-  ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
-  ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
-  ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
-  ["Secret pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
+  ["Daycare Egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
+  ["Secret Pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+  ["Secret Pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+  ["Secret Pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+  ["Secret Pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
+  ["Royalty Charm"] = {Class = "Charm", Id = "Royalty", MinAmount = 1},
+  ["Overload Charm"] = {Class = "Charm", Id = "Overload", MinAmount = 1},
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
