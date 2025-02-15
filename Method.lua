0ebe1babcae800ce4f5d463f035ffe041a97b9b4
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-02-15 17:53:34 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,7 +1,10 @@
 -- Please Read FAQ channel for more info! Make sure you understand these parameters
 -- If not sure ask info in channel
 script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO";
-_G.GPROGRESS_MODE = "Hybrid"
+_G.GDO_TOWER_TYCOON_EVENT = true
+_G.GCONVERT_VALENTINE_HEARTS = true
+_G.GUSE_TOWER_TYCOON_BOOSTS = true
+_G.GAUTO_TYCOON_PET_UPGRADE = true
 _G.GGFX_MODE = 1
 _G.GHATCH_CHARGED_EGGS = false
 _G.GUSE_ULTIMATES = {"Chest Spell","UFO"} -- default is Ground Pound, ultimates to try to equip at script load.
@@ -18,6 +21,7 @@ _G.GWEBHOOK_USERID = "667064890359545917"
 _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1222557363874168983/cd_SGmpFvesnVoiclLSyost8Ff7Yh83JApCqFtqpENfh2A2WUJIIpuTcE7JaxTzZ3hn0"
 _G.GMAIL_RECEIVERS = {"ProfiAzUr"} -- for Huges & items
 _G.GMAIL_ITEMS = {
+  ["Love Gift"] = {Class = "Lootbox", Id = "Love Gift", MinAmount = 500},
   ["Daycare Egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
   ["Secret Pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
   ["Secret Pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
