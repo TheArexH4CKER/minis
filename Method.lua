76e18e4a22d71a6f954a792d109070b9f0bc5326
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-18 17:31:49 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,52 +1,34 @@
--- Please Read FAQ channel for more info! Make sure you understand these parameters
--- If not sure ask info in channel
-script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO";
-_G.GPROGRESS_MODE = "Hybrid"
-_G.GGFX_MODE = 1
-_G.GDO_LUCKY_WORLD_EVENT = true
-_G.GAUTO_RAID = true
-_G.GDO_BOSSES = true
---_G.GRESET_UPGRADES = true
-_G.GUSE_BOOSTS = true
-_G.GUSE_LEPRECHAUN_KEYS = true
-_G.GMAX_RAID_LEVEL = 5000
-_G.GMIN_MULTIPLIER = 300 -- which egg to hatch
-_G.GEVENT_UPGRADES = {
- "LuckyRaidDamage",
- --"LuckyRaidAttackSpeed",
- "LuckyRaidPets",
- "LuckyRaidEggCost",
- "LuckyRaidMoreCurrency",
- "LuckyRaidBetterLoot",
- "LuckyRaidTitanicChest",
- "LuckyRaidHugeChest",
- "LuckyRaidXP",
- "LuckyRaidPetSpeed",
- "LuckyRaidBossDamage",
- "LuckyRaidKeyDrops",
- "LuckyRaidBossHugeChances",
- "LuckyRaidBossTitanicChances",
+getgenv().Configuration = {
+    ['Auto Mail'] = {
+        ['Items'] = {
+            ['Lootbox'] = {
+                ['Lucky Gift'] = { ['Amount'] = 9999999 },
+            },
+            ['Misc'] = {
+                ['Leprechaun Key'] = { ['Amount'] = 99999999 },
+                ['Lucky Raid Boss Key'] = { ['Amount'] = 9999999 },
+            },
+        },
+        ['Mail All Huges'] = false,
+        ['Usernames'] = { "" },
+    },
+    ["Webhook"] = {
+        ["UserID"] = "667064890359545917",
+        ["WebhookURL"] = "https://discord.com/api/webhooks/1249413828547182742/VUCfE51nqu43KBRgI1qqyhoZd7AeGZtrQgsr1AADWAABe1xSFk62t77zHTCJtGqFCzk3",
+    },
+    ['Event'] = {
+        ['Raid Settings'] = {
+            ['Enabled'] = true,
+            ['Max Difficulty'] = 5000,
+            ['Egg Multiplier'] = 9900,
+            ['Max Raid Time'] = 60,
+            ['Raid Bosses'] = {true, true, false}, -- Enabled, Heroic, Use Required Item Doors
+            ['Leave Breakables'] = {"LuckyRaidMassiveChest", "LuckyRaidPotOfGoldChest"}, -- LuckyRaidMediumChest, etc
+        },
+        ['Craft Gift'] = false,
+        ['Upgrades'] = { "LuckyRaidXP", "LuckyRaidPets", "LuckyRaidDamage", "LuckyRaidAttackSpeed" },
+    },
+    --['Debug'] = {false, false},
+    ['Equip Enchants'] = {},
 }
-_G.GLOOTBOXES = {"Lucky Gift"}
-_G.GHATCH_CHARGED_EGGS = false
-_G.GUSE_ULTIMATES = {"Chest Spell","UFO"} -- default is Ground Pound, ultimates to try to equip at script load.
-_G.GRANK_TO = 33
-_G.GZONE_TO = 999 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
-_G.GMAX_EGG_SLOTS = 99
-_G.GMAX_EQUIP_SLOTS = 99
-_G.GHOLD_GIFTS = false
-_G.GHOLD_BUNDLES = false
-_G.GPOTIONS = {"Coins","Lucky","The Cocktail","Treasure Hunter","Walkspeed","Diamonds","Damage"}
-_G.GENCHANTS = {}
-_G.GFRUITS = {"Apple","Banana","Orange","Rainbow","Pineapple","Watermelon"}
-_G.GWEBHOOK_USERID = "667064890359545917"
-_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1222557363874168983/cd_SGmpFvesnVoiclLSyost8Ff7Yh83JApCqFtqpENfh2A2WUJIIpuTcE7JaxTzZ3hn0"
-_G.GMAIL_RECEIVERS = {"ProfiAzUr"} -- for Huges & items
-_G.GMAIL_ITEMS = {
-  ["Daycare Egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
-  ["Secret Pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
-  ["Secret Pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
-  ["Secret Pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
-  ["Secret Pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b0dec084ff36c27d9dc119ad6b094544.lua"))()
