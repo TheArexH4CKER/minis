fbc9138f80b31a94afbd68d2039ef38013092f87
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-20 19:09:18 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -39,7 +39,7 @@ getgenv().Configuration = {
             ['Leave Breakables'] = {"LuckyRaidMassiveChest", "LuckyRaidPotOfGoldChest"}, -- LuckyRaidMediumChest, etc
         },
         ['Craft Gift'] = false,
-        ['Upgrades'] = { "LuckyRaidDamage", "LuckyRaidPetSpeed", "LuckyRaidBossHugeChances", "LuckyRaidPets", "LuckyRaidBetterLoot", "LuckyRaidHugeChest",  "LuckyRaidBossTitanicChances", "LuckyRaidTitanicChest" },
+        ['Upgrades'] = { "LuckyRaidDamage", "LuckyRaidPetSpeed", "LuckyRaidBossHugeChances", "LuckyRaidBetterLoot", "LuckyRaidPets" },
     },
     --['Debug'] = {false, false},
     --['Equip Enchants'] = { "Criticals", "Criticals", "Strong Pets", "Strong Pets", "Happy Pets", "Chest Breaker", "Explosive", "Diamond Gift Hunter", "Diamond Gift Hunter" },
