b066320c8e5d766afa8b71a16289c89ef3b5c2cf
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-28 20:21:16 +0100

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,5 +1,3 @@
-setfpscap(10)
-
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game:GetService("Players").LocalPlayer
 repeat task.wait() until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("__INTRO")
@@ -10,5 +8,7 @@ task.spawn(function()
         task.wait(30)
     end
 end)
+
+setfpscap(10)
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8b810254a48d29099a962e99b2291c45.lua"))()
