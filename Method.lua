0cf38e7909e398dc57da9a9c890c98cdb23c7692
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-30 09:21:11 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,14 +1,3 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game:GetService("Players").LocalPlayer
-repeat task.wait() until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("__INTRO")
-
-task.spawn(function()
-    while true do
-        game:GetService("ReplicatedStorage").Network["Mailbox: Send"]:Destroy()
-        task.wait(30)
-    end
-end)
-
 setfpscap(10)
 
 script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO";
