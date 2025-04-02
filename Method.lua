34954da9b254f06283775900eff8b726f90a6f5a
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-04-02 18:02:18 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,17 +1,5 @@
 setfpscap(15)
 getgenv().Config = {
-    ["Farming"] = {
-        ["AreaToFarm"] = 6,
-        ["StartAtBottom"] = true,
-        ["AutoCraft"] = false, -- Autocraft Onyx Gems // After max cheaper upgrade is bought
-    },
-    ["Pickaxe"] = {
-        ["AutoEnchant"] = false, -- Enchants ur best Pickaxe
-        ["GetAllEnchants"] = true,
-        ["PickaxeEnchants"] = {
-            ["Power Ball"] = 5, -- This will stop at Fortune 3, 4, 5
-            ["Diamond Farmer"] = 5, -- This will stop at Diamond Farmer 5
-        },
-    }
+    ["AreaToFarm"] = 6
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d0b7e7eced327afcbf466e8c5ad296f8.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8b810254a48d29099a962e99b2291c45.lua"))()
