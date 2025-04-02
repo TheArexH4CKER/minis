88ebb01c988c5d60e75dde514272fa9929b149f5
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-04-02 21:30:47 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,7 +1,7 @@
 setfpscap(20)
 getgenv().Config = {
     ["Farming"] = {
-        ["AreaToFarm"] = 6,
+        ["AreaToFarm"] = 5,
         ["StartAtBottom"] = true,
         ["AutoCraft"] = false, -- Autocraft Onyx Gems // After max cheaper upgrade is bought
     },
