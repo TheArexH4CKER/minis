17e7075fe02087127192e9561b62185da65797d5
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-04-03 07:38:13 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,5 +1,5 @@
 setfpscap(20)
 getgenv().Config = {
-    ["AreaToFarm"] = 5
+    ["AreaToFarm"] = 6
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8b810254a48d29099a962e99b2291c45.lua"))()
