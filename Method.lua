227940f8eb6b2b848e490c4b1361567b3f0aebd1
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-04-05 18:53:31 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,5 +1,25 @@
-setfpscap(20)
-getgenv().Config = {
-    ["AreaToFarm"] = 6
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8b810254a48d29099a962e99b2291c45.lua"))()
+script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO";
+ _G.GPROGRESS_MODE = "Hybrid"
+ _G.GGFX_MODE = 1
+ _G.GHATCH_CHARGED_EGGS = false
+ _G.GUSE_ULTIMATES = {"Chest Spell","UFO"} -- default is Ground Pound, ultimates to try to equip at script load.
+ _G.GRANK_TO = 33
+ _G.GZONE_TO = 999 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
+ _G.GMAX_EGG_SLOTS = 99
+ _G.GMAX_EQUIP_SLOTS = 99
+ _G.GHOLD_GIFTS = false
+ _G.GHOLD_BUNDLES = false
+ _G.GPOTIONS = {"Coins","Lucky","The Cocktail","Treasure Hunter","Walkspeed","Diamonds","Damage"}
+ _G.GENCHANTS = {}
+ _G.GFRUITS = {"Apple","Banana","Orange","Rainbow","Pineapple","Watermelon"}
+ _G.GWEBHOOK_USERID = "667064890359545917"
+ _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1222557363874168983/cd_SGmpFvesnVoiclLSyost8Ff7Yh83JApCqFtqpENfh2A2WUJIIpuTcE7JaxTzZ3hn0"
+ _G.GMAIL_RECEIVERS = {"ElfBarZero","ProfiAzUr"} -- for Huges & items
+ _G.GMAIL_ITEMS = {
+   ["Daycare Egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
+   ["Secret Pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+   ["Secret Pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+   ["Secret Pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+   ["Secret Pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
+ }
+ loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
