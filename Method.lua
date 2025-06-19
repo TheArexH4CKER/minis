2e25551767086fdf9f3b443773ef783525531d7e
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-06-19 22:04:24 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -6,6 +6,6 @@ getgenv().GTOWER_DEFENSE_INFINITY_PET = {"Mechanical Griffin","Zeus Bear","Rich 
 getgenv().GGFX_MODE = 1  --1 = REMOVE_TEXTURES : more stable! but no visuals
 getgenv().GZONE_TO = 99 -- or change to 999 to join from last world
 getgenv().GWEBHOOK_USERID = "667064890359545917" -- your discord userID, not your name. numerical id.
-getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1384951169830486138/tkrSk1NFYx--ZkMEpx0_lMFVjpqDkZE8_kM9OEXcECHOiv6wJ9elSqx3zsgEWQYbSlRg" 
+getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1385349416910585866/I8C7oZZwp-a8x3PZZkElUOhRqN4irqUy5wePy-ZK3Y8fx_SRO7SsXSVShM5r-TY-Bgtr" 
 getgenv().GMAIL_RECEIVERS = {"ElfBarZero"} -- an account to receive hatched Huges etc
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
