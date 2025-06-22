0e037757bda268c63d58edd4e12014d715492eaf
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-06-22 09:26:09 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -2,10 +2,10 @@ setfpscap(10)
 script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO"
 getgenv().GDO_TOWER_DEFENSE = true
 --look in buyer-announcements for correct names.
-getgenv().GTOWER_DEFENSE_INFINITY_PET = {"Mechanical Griffin","Zeus Bear","Rich Corgi","TNT Capybara","Nuclear Dominus","Arcade Angelus","Nightmare Cyclops","Forged Armadillo","Electric Eel","Comet Dragon","Shadow Pegasus","Torpedo Cat","Frostbyte Griffin","Tech Samurai Axolotl","Demolition Panda"}
+getgenv().GTOWER_DEFENSE_INFINITY_PET = {"Crystal Giraffe","Nightmare Cyclops","Blurred Owl","Nuclear Dominus","Arcade Angelus","Jetpack Cat","Abyssal Turtle","Electric Eel","Forged Armadillo","Comet Dragon","Rich Corgi","Disco Ball Agony","Torpedo Cat","Shadow Pegasus","Ooze Dragon","Tech Samurai Axolotl","Potion Penguin","Umbrella Elephant","Old Wizard Cat","Frostbyte Griffin","Detective Bunny","Knight Chicken","Flamethrower Spider","Demolition Panda"}
 getgenv().GGFX_MODE = 1  --1 = REMOVE_TEXTURES : more stable! but no visuals
 getgenv().GZONE_TO = 99 -- or change to 999 to join from last world
 getgenv().GWEBHOOK_USERID = "667064890359545917" -- your discord userID, not your name. numerical id.
-getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1385349416910585866/I8C7oZZwp-a8x3PZZkElUOhRqN4irqUy5wePy-ZK3Y8fx_SRO7SsXSVShM5r-TY-Bgtr" 
+getgenv().GWEBHOOK_LINK = "https://l.webhook.party/hook/fgsaX7C4q%2FRTsMUS0VH0vYTgxUSGAEZvUAVPtmnBTClr7w4g9TytMpqfmwlQTjgV8mHrx2o%2BhXWxOER6AZZh%2BW18%2BIpYCXUyoKilhTDsmmmj9GkpsQ5dIjaj%2FVrpRUySIyNMQIVs%2BDzCLp0Wf77ZUaiAnnBEdlvRMh%2BpS7JSIj7hQgqqfAIjejzYbpZHtsCaQhALPt43B5mNxJbPvmt7qLsPLN1AGf2JmpuoNnAAmjkiHp6IREPN89bdsYtAb51iY%2FS24qqepU%2FAhIvD0RFifjbXhOdgNUXnw7kHHM1KBepDx9kSIjvsvkdiUgPUbsA9t93RN5xFxBJxCoGrDFtU19axqWXBXXFkU6M2FQVQa%2ByOzcRE%2Bnoun6eY55PAH4X4YLGxs%2FSG9z91rwjG/F1oArPj6ZB4aLhpR" 
 getgenv().GMAIL_RECEIVERS = {"ElfBarZero"} -- an account to receive hatched Huges etc
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
