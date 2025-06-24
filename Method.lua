d3f2dc69865409b0557957216354da633220c428
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-06-24 22:12:21 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -7,5 +7,5 @@ getgenv().GGFX_MODE = 1  --1 = REMOVE_TEXTURES : more stable! but no visuals
 getgenv().GZONE_TO = 99 -- or change to 999 to join from last world
 getgenv().GWEBHOOK_USERID = "667064890359545917" -- your discord userID, not your name. numerical id.
 getgenv().GWEBHOOK_LINK = "https://l.webhook.party/hook/fgsaX7C4q%2FRTsMUS0VH0vYTgxUSGAEZvUAVPtmnBTClr7w4g9TytMpqfmwlQTjgV8mHrx2o%2BhXWxOER6AZZh%2BW18%2BIpYCXUyoKilhTDsmmmj9GkpsQ5dIjaj%2FVrpRUySIyNMQIVs%2BDzCLp0Wf77ZUaiAnnBEdlvRMh%2BpS7JSIj7hQgqqfAIjejzYbpZHtsCaQhALPt43B5mNxJbPvmt7qLsPLN1AGf2JmpuoNnAAmjkiHp6IREPN89bdsYtAb51iY%2FS24qqepU%2FAhIvD0RFifjbXhOdgNUXnw7kHHM1KBepDx9kSIjvsvkdiUgPUbsA9t93RN5xFxBJxCoGrDFtU19axqWXBXXFkU6M2FQVQa%2ByOzcRE%2Bnoun6eY55PAH4X4YLGxs%2FSG9z91rwjG/F1oArPj6ZB4aLhpR" 
-getgenv().GMAIL_RECEIVERS = {"Sebastian_Hero74", "Gabri3lBlast44", "Harp3rLuckySpark", "KAYL33OM3GAAQUA_YT", "XxIsab3llaFlashxX89", "XxCodeRiftZapxX2008", "XxBlockSonicxX2009", "EliKnightKing", "LiamSparklyBlade", "NoahStarCraft", "Haze_Rogue2013", "ByteIceLegend80"}
+getgenv().GMAIL_RECEIVERS = {"Gabri3lBlast44", "Harp3rLuckySpark", "KAYL33OM3GAAQUA_YT", "XxIsab3llaFlashxX89", "XxCodeRiftZapxX2008", "XxBlockSonicxX2009", "EliKnightKing", "LiamSparklyBlade", "NoahStarCraft", "Haze_Rogue2013", "ByteIceLegend80"}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
