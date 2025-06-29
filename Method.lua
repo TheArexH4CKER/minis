6b851899d8d2b563cb94bf7d6c37777000b6cc32
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-06-29 16:43:13 +0200

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -3,6 +3,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/TheArexH4CKER/minis/r
 task.wait(5)
 
 script_key = "oienRcMcQXAagTUfNqGIizJioerDUgCO"
+
 getgenv().GDO_GYM_EVENT = true
 getgenv().GGYM_EVENT_STAT_CONFIG = {
     ["Stamina"] = 30,
@@ -10,13 +11,19 @@ getgenv().GGYM_EVENT_STAT_CONFIG = {
     ["Strength"] = 62,
     ["Size"] = 1
 }
+
 getgenv().GGFX_MODE = 1  --1 = REMOVE_TEXTURES : more stable! but no visuals
+
+getgenv().GRANK_FIRST = true
 getgenv().GZONE_TO = 99 -- or change to 999 to join from last world
 getgenv().GRANK_TO = 6
+getgenv().GPROGRESS_MODE = "Hybrid" -- Hatch, Hybrid, etc.
+
 getgenv().GENCHANTS = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Lucky Eggs"}
 getgenv().GPOTIONS = {"Lucky"}
 getgenv().GUSE_BOOSTS = true
 getgenv().GLOOTBOXES = {"Locked Hype Egg"}
+
 getgenv().GWEBHOOK_USERID = "667064890359545917" -- your discord userID, not your name. numerical id.
 getgenv().GWEBHOOK_LINK = "https://l.webhook.party/hook/fgsaX7C4q%2FRTsMUS0VH0vYTgxUSGAEZvUAVPtmnBTClr7w4g9TytMpqfmwlQTjgV8mHrx2o%2BhXWxOER6AZZh%2BW18%2BIpYCXUyoKilhTDsmmmj9GkpsQ5dIjaj%2FVrpRUySIyNMQIVs%2BDzCLp0Wf77ZUaiAnnBEdlvRMh%2BpS7JSIj7hQgqqfAIjejzYbpZHtsCaQhALPt43B5mNxJbPvmt7qLsPLN1AGf2JmpuoNnAAmjkiHp6IREPN89bdsYtAb51iY%2FS24qqepU%2FAhIvD0RFifjbXhOdgNUXnw7kHHM1KBepDx9kSIjvsvkdiUgPUbsA9t93RN5xFxBJxCoGrDFtU19axqWXBXXFkU6M2FQVQa%2ByOzcRE%2Bnoun6eY55PAH4X4YLGxs%2FSG9z91rwjG/F1oArPj6ZB4aLhpR" 
 getgenv().GMAIL_RECEIVERS = {"ElfBarZero"}
